0f6830a101b23c84409333e4d13ddb450d02d98d
Carla Florida <carli.f.roman@gmail.com>
2024-05-24 08:15:34 -0500

    Query to PDF and DAtabase

--- multi_agent.py
+++ multi_agent.py
@@ -1,6 +1,3 @@
-import os
-from glob import glob
-import pandas as pd
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
@@ -17,6 +14,12 @@ from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe
 from langchain_openai import ChatOpenAI
 import pandas as pd
 from langchain.retrievers import (MergerRetriever,)
+import os
+from glob import glob
+import pandas as pd
+from langchain_community.utilities import SQLDatabase
+from sqlalchemy import create_engine
+from langchain_community.agent_toolkits import create_sql_agent
 
 llm = ChatOpenAI(model_name="gpt-4-turbo", temperature=0)
 
@@ -34,6 +37,23 @@ def CSV_agent(df, query):
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,)
     agent.invoke(query)
+
+def consultar_datos_por_municipio(consulta, query, municipio):
+    print("step 1")
+
+    print("step 2")
+
+    engine = create_engine(
+        "postgresql://clients:bZqbYHeEA2x$Qa6KxnYVkX@clients.cluster-c9w8clqzjjhu.us-east-2.rds.amazonaws.com:5432/fip_db_ai_model"
+    )
+
+    db = SQLDatabase(engine=engine)
+    print(db.get_usable_table_names())
+
+    agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
+    response = agent_executor.invoke(f"{query}, Municipio: {municipio}")
+    print(response)
+    return response
 
 #Función para cargar múltiples bases de datos desde Pinecone
 def load_bases():
@@ -75,20 +95,59 @@ def get_general_information_from_PDFs(consulta: str) -> str:
     retriever_chain = get_retriever(big_vector)
     return retriever_chain.invoke(consulta)
 
+@tool
+def get_my_information_from_database(consulta: str, municipio:str) -> str:
+    """Consultar información de los delitos desde el CSV"""
+    return consultar_datos_por_municipio("",consulta, municipio)
+
+
 #Build chain
-def Chain_with_tool():
-    tools = [get_crime_information_from_CSVs, get_general_information_from_PDFs]
+def Chain_with_tool(municipio):
+    tools = [get_my_information_from_database, get_general_information_from_PDFs]
 
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                """
-                Eres un asistente diseñado para responder preguntas sobre la creación de Planes Integrales de Seguridad y Convivencia Ciudadana (PISCC).
+                f"""
+                Eres un asistente diseñado para responder preguntas sobre la creación de Planes Integrales de Seguridad y Convivencia Ciudadana (PISCC) del municipio de {municipio}.
                     1. Información sobre delitos: Si deseas obtener información sobre los delitos en un municipio, puedo utilizar los datos de los archivos CSV disponibles.    
                     2. Información general de documentos de política pública: Si deseas obtener información general de creación de PISCC, leyes o política pública, puedo utilizar la base vectorial de Pinecone. 
                     Puedo utilizar las herramientas ("TOOLS") disponibles para proporcionar la información indicada que necesitas para crear un documento PISCC para mi municipio.
-                    IMPORTANTE: la columna llamada "Estadísticas (1)" representa delitos
+                    IMPORTANTE: 
+                    - La columna llamada "Estadísticas (1)" representa delitos.
+                    - En caso de que no se especifique el municipio, se asumirá que se está hablando de {municipio}.
+                    
+                    ###
+                    Municipios disponibles: Input === Nombre de columna en el CSV
+                    Bogotá === Bogotá, D.C. Cap.
+                    Abrego === Ábrego (Nsa)
+                    Bolivar === Bolívar (Vac)
+                    Buenavista === Buenavista (Cor)
+
+                    ###
+                    Columnas.
+                    Delitos === Estadísticas (1)
+
+                    ###
+                    CATERGORIAS DE DELITOS:
+                    
+                    Delito sexual === Tema: "Agresiones contra la Integridad Personal" | Estadísticas (1): "Delito Sexual ( Exámenes medicolegales )"
+                    Extorsión === Tema: "Secuestro _ Extorsión" | Estadísticas (1): "Extorsión ( Personas Víctimas )"
+                    Homicidio (incluye abatidos) === Tema: "Agresiones contra la Integridad Personal" | Estadísticas (1): "idio - incluye abatidos (INML) ( Personas Víctimas )"
+                    Homicidio (Totales) === Tema: "Agresiones contra la Integridad Personal" | Estadísticas (1): "Homicidio - Totales (Min Def) ( Personas Víctimas )"
+                    Hurto a personas === Tema: "Hurto _ Delitos contra el patrimonio económico" | Estadísticas (1): "Hurto a Personas ( Personas Víctimas )"
+                    Hurto a comercios === Tema: "Hurto _ Delitos contra el patrimonio económico" | Estadísticas (1): "Hurto a Comercios ( Personas Víctimas )"
+                    Hurto a residencias === Tema: "Hurto _ Delitos contra el patrimonio económico" | Estadísticas (1): "Hurto a Residencias ( Personas Víctimas )"
+                    Hurto a automoviles === Tema: "Hurto _ Delitos contra el patrimonio económico" | Estadísticas (1): "Hurto de Automotores ( Unidades )"
+                    Lesiones violencia interpersonal === Tema: "Agresiones contra la Integridad Personal" | Estadísticas (1): "Lesiones Violencia Interpersonal ( Personas Víctimas )"
+                    Secuestro === Tema: "Secuestro _ Extorsión" | Estadísticas (1): "Secuestro ( Personas Víctimas )"
+                    Violencia intrafamiliar === Tema: "Agresiones contra la Integridad Personal" | Estadísticas (1): "Violencia Intrafamiliar ( Personas Víctimas )"
+        
+                    ###
+                    Ejemplo de consulta:
+                    Input === "3 delitos más frecuentes el año 2021 en Bogotá" ||| municipio === "Bogotá D.C." query === "3 delitos más frecuentes el año 2021"
+                    Output: "Los 3 delitos más frecuentes en Bogotá D.C. el año 2021 son: ..."
                 """,
             ),
             ("placeholder", "{conversation_history}"),
@@ -100,8 +159,8 @@ def Chain_with_tool():
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     return agent_executor
 
-test_agent = Chain_with_tool()
+test_agent = Chain_with_tool(municipio="Ábrego (Nsa)")
 
 #query = "Cómo construir un piscc?"
-query = "cuantos municipios existen en la base de datos?"
+query = "Delito más frecuentes el año 2021 en Bogotá"
 print(test_agent.invoke({"conversation_history": [], "input": query}))
